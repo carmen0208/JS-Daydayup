9db47c2f86b1616057633bf43350210114a0a0fe
Carmen Liu <carmen.liujia@gmail.com>
2016-12-27 18:42:19 +1300

    high order examples

--- higher-order-functions/high-order-return.js
+++ higher-order-functions/high-order-return.js
@@ -7,3 +7,37 @@ function greaterThan(m) {
 }
 
 console.log(`High Order:greaterThans: ${x} > ${y} :` + greaterThan(x)(y))
+
+console.log("FUNCTION: that change other function:");
+function noisy(f) {
+  return function(arg) {
+    console.log(f, "calling with ", arg);
+    var val = f(arg);
+    console.log(f, "calling with ", arg, "- got", val);
+  };
+}
+
+noisy(Boolean)(0);
+
+noisy(greaterThan(x))(20);
+
+console.log("FUNCTION: provide new types of control flow:");
+
+function unless(test, then) {
+  if(!test) {
+    then();
+  }
+}
+
+function repeat(times, body) {
+  for(var i=0; i< times; i++) {
+    body(i);
+  }
+}
+
+
+repeat(3, function(n) {
+  unless( n%2, function(){
+    console.log(n, "is even")
+  });
+});
