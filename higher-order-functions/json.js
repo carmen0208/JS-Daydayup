c670ed1afb7af5a872d7b0df00f8f1885af14ad1
Carmen Liu <carmen.liujia@gmail.com>
2016-12-27 19:00:58 +1300

    json

---
+++ higher-order-functions/json.js
@@ -0,0 +1,7 @@
+var string = JSON.stringify({name:"X", born: 1980})
+console.log(string);
+console.log(JSON.parse(string).born);
+
+
+var ancestry = JSON.parse(ANCESTRY_FILE);
+console.log(ancestry.length);
