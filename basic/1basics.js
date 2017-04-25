4a0fd58bbd90777b13ec0003993c6943c416ef58
Carmen Liu <carmen.liujia@gmail.com>
2017-04-25 16:40:59 +1200

    add some really basic things

---
+++ basic/1basics.js
@@ -0,0 +1,14 @@
+var y = 3;
+var z = '2';
+var answer = parseInt(z) + y;
+console.log(typeof answer);
+console.log(typeof "asd");
+console.log(typeof true);
+console.log(typeof console);
+console.log(typeof console.log);
+
+var math = 6 * Math.random();
+console.log(math);
+console.log(Math.round(math));
+console.log(Math.ceil(math));
+console.log(Math.floor(math));
